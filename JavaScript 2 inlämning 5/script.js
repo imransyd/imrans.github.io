4b25f0f02037ed4b6347afcb10a31405b0fd942b
Imran Syed <imransyd.88@gmail.com>
2017-04-23 13:24:22 +0200

    Update script.js

--- JavaScript 2 inlämning 5/script.js
+++ JavaScript 2 inlämning 5/script.js
@@ -1,38 +1,26 @@
-/*jshint esnext: true, moz: true*/
-/*jslint browser:true */
-/*global firebase, React, ReactDOM */
-
-
 var React = require('react');
 var ReactDOM = require('react-dom');
+
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			msg1: '',
-			msg2: ''
-		}
-		this.handleKeyUp1 = this.handleKeyUp1.bind(this);
-		this.handleKeyUp2 = this.handleKeyUp2.bind(this);
+			msg: ''
+		};
+		this.handleKeyUp = this.handleKeyUp.bind(this);
 	}
-	handleKeyUp1(event) {
-		console.log('handleKeyUp1');
+	handleKeyUp(event) {
+		console.log('handleKeyUp');
 		this.setState({
-			msg2: event.target.value
-		})
-	}
-	handleKeyUp2(event) {
-		console.log('handleKeyUp2');
-		this.setState({
-			msg1: event.target.value
-		})
+			msg: event.target.value
+		});
 	}
 	render() {
 		return (
 		  <div className="myform">
-			<MyForm handleKeyUp={this.handleKeyUp1} msg={this.state.msg1} />
-			<MyForm handleKeyUp={this.handleKeyUp2} msg={this.state.msg2} />
+		  <h2>* 1st part</h2>
+			<MyForm handleKeyUp={this.handleKeyUp} msg={this.state.msg} />
 		  </div>
 		);
 	}
